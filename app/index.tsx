af795d3cc1b97f29d1b02dbe15707182b80cd4b0
baudrysouvignet <baudrysouvignet07@gmail.com>
2025-03-03 09:00:58 +0100

    start

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import * as Contacts from 'expo-contacts';
+
+
+export default function Index() {
+  
+
+  return (
+    <View >
+      <Text>OK 12</Text>
+    </View>
+  );
+}
